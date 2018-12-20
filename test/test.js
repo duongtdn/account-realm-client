0ab0d117933ad7a6aa3c79bcbf9b7f0c273a9a23
duongtdn <duongtdn@gmail.com>
2018-12-20 03:21:34 +0000

    add temporary test script

---
+++ test/test.js
@@ -0,0 +1,34 @@
+"use strict"
+
+const args = process.argv.slice(2)
+if (args.length > 0) {
+  console.log('# Test arguments')
+  args.forEach(arg => {
+    console.log(`   - ${arg}`)
+  });
+}
+
+
+// const AuthProvider = require("../dist/auth-provider")
+
+// const auth = new AuthProvider({
+//   baseurl: 'https://account-realm.com'
+// })
+
+// // auth.get('session/new', {template: 'default', theme: 'material'})
+
+// // auth.get('session/new', function(err, data) {
+// //   console.log(data)
+// // })
+
+// auth.get('session/new', {template: 'default', theme: 'material'}, function(err, data) {
+//   console.log(data)
+// })
+
+const AccountClient = require("../dist/account-client")
+
+const acc = new AccountClient({
+  baseurl: 'https://account-realm.com'
+})
+
+acc.sso()

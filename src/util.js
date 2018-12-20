b2fc1160ee5be777e524746f04b80b4bbeaf791a
duongtdn <duongtdn@gmail.com>
2018-12-20 03:24:29 +0000

    add more typecheck functions

--- src/util.js
+++ src/util.js
@@ -3,8 +3,8 @@
 const getClass = {}.toString
 const util = {}
 
-['Arguments', 'Array', 'Function', 'String', 'Number', 'Date', 'RegExp', 'Object']
-.forEach( name => {
+const types = ['Arguments', 'Array', 'Function', 'String', 'Number', 'Date', 'RegExp', 'Object']
+types.forEach( name => {
   util[`is${name}`] = function (obj) {
     return obj && getClass.call(obj) == `[object ${name}]`;
   };

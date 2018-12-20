d21b4cc177c68244f9fe1a433b946327248201f4
duongtdn <duongtdn@gmail.com>
2018-12-20 09:39:54 +0000

    add tests for type check

--- tests/util.test.js
+++ tests/util.test.js
@@ -1,14 +1,33 @@
 "use strict"
 
-describe('Test util lib', () => {
-  it('should succeed', (done) => {
-    setTimeout(done, 1000)
-  })
+import { assert } from 'chai'
 
-  it('should fail', () => {
-    setTimeout(() => {
-      throw new Error('Failed')
-    }, 1000)
+import util from '../src/util'
+
+describe('Test util lib', () => {
+
+  describe('Type check', function() {
+    const obj = {
+        Arguments : arguments,
+        Array : [],
+        Function : function () {},
+        String : 'it is a string',
+        Number : 123,
+        Date : new Date(),
+        RegExp : new RegExp("ab+c"),
+        Object : {}
+    }
+    const types = ['Arguments', 'Array', 'Function', 'String', 'Number', 'Date', 'RegExp', 'Object'];
+    types.forEach( type => {
+      describe(`is${type}`, function () {            
+        types.forEach( name => {
+          it (`Check ${type.toUpperCase()} as ${name.toUpperCase()}`, function() {
+            assert.equal( util[`is${name}`](obj[type]), (type === name) );
+          })
+        });    
+      });  
+    });
+  
   })
 
 })

eabe6394a64e9027812423736e6eadc2c1d417ee
duongtdn <duongtdn@gmail.com>
2018-12-24 07:36:56 +0000

    rename for devtest

--- tests/account-client.devtest.js
+++ tests/account-client.devtest.js
@@ -21,7 +21,7 @@ const auth = new AuthProvider({
 //   console.log(data)
 // })
 
-auth.get('apps/test/session', {template: 'default', theme: 'material'}, function(data) {
+auth.get('apps/dev/session', {template: 'default', theme: 'material'}, function(data) {
   console.log(data)
 })
 

0d76fbb678f84ff4396cc12859e4fc51c106d21d
duongtdn <duongtdn@gmail.com>
2018-12-25 07:09:05 +0700

    add event listerner to window postMessage when creating object, update callback whenever get function is called so that event will not be triggered twice

--- src/auth-provider.js
+++ src/auth-provider.js
@@ -7,26 +7,28 @@ import { get } from './xhttp'
 export default class AuthProvider {
   constructor({ baseurl }) {
     this.baseurl = baseurl.replace(/\/+$/,'')
-    this._lazyFn = [];
+    this._lazyFn = []
+    this._done = null
     document.addEventListener("DOMContentLoaded", (event) => {
       this._domReady = true
       this._lazyFn.forEach(f => f.fn(...f.args))
-    });
+    }, false)
+    window.addEventListener("message", (event) => {
+      if (event.origin !== this.baseurl)
+        return
+      const data = event.data
+      this._done && this._done(data)
+    }, false)
   }
 
-  get(path, query, done) {    
+  get(path, query, done) {   
     if (isFunction(query)) {
       done = query
     }
     const url = this._constructURL(path, query)
+    console.log(`GET ${url} HTTP / 1.1`) 
     this._lazyExecute(this._openIframe, url)
-    window.addEventListener("message", receiveMessage.bind(this), false);    
-    function receiveMessage (event) {
-      if (event.origin !== this.baseurl)
-        return;
-      const data = event.data
-      done && done(data)
-    }    
+    this._done = done  
   }
 
   delete(path, done) {
@@ -55,9 +57,7 @@ export default class AuthProvider {
 
   _openIframe(url) {   
     let div = document.getElementById(`__${this.baseurl}__container__`)
-    console.log(div)
     if (!div) {
-      console.log('create div')
       div = document.createElement('div')
       div.setAttribute('id', `__${this.baseurl}__container__`)
       document.getElementsByTagName('body')[0].appendChild(div)

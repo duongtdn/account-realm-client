492af982d41762db0d71fd00977e8303d1e4b8df
duongtdn <duongtdn@gmail.com>
2018-12-26 22:32:04 +0700

    add signup form

--- tests/server/template.js
+++ tests/server/template.js
@@ -37,6 +37,51 @@ const html = {
     
     </html>
     `
-  } 
+  },
+
+  signup () {
+    return `
+    <!DOCTYPE html>
+    <html>
+    
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1,  shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        
+        <title>Accounts</title>      
+           
+        <link rel="stylesheet" type="text/css" href="https://www.w3schools.com/w3css/4/w3.css">
+    
+        <style>
+    
+        </style>
+    
+      </head>
+    
+      <body>    
+
+        <h2> Create new account </h2>
+    
+        <form action="/users/new" class="w3-container" method="post">
+          <p>
+            <label> Username </label>
+            <input class="w3-input " type="text" name="username" placeholder="Please enter username" />
+          </p>
+          <p>
+            <label> Password </label>
+            <input class="w3-input " type="password" name="password" placeholder="Please enter password" />
+          </p>
+          
+          <input type="submit" value="Submit" class="w3-button w3-blue" />
+        </form>
+    
+      </body>
+    
+    </html>
+    `
+  }
 
 }
+
+module.exports = html
